02ac5d3821c4ef8b94691f85049a23d008bc53be
Michael <mr.michaelwitter@gmail.com>
2020-03-30 13:19:55 -0400

    java CheckPromise class

---
+++ Java Practice/CheckPromise.java
@@ -0,0 +1,47 @@
+public class CheckPromise {
+	
+	public static void main(String [] args) {
+
+	
+		int number = 98;
+		
+
+		
+		if (isPromise(number)) {
+			
+			System.out.println("It is a prime number");
+		}
+		else {
+			System.out.println("It is not a prime number");
+		}
+		
+		
+	    
+	}
+	
+	public static boolean isPromise(int x) {
+		
+		boolean check = (x == 1 || x == 0) ? false: true;
+		
+		
+		
+		if (x > 1) {
+			
+			for (int i = 2; i < x; i++) {
+				
+				if ((x % i == 0) && (i != x)) {
+					System.out.println("Prime check found that " + x + " is divisible at " + i);
+					
+					check = false;
+				}
+				
+
+				
+			}
+		}
+		
+		return check;
+	}
+	
+	
+}
