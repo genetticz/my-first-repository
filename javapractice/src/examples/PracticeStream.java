b04f3db822db1846be4720c91e52e85000f8c41b
genetticz <mr.michaelwitter@gmail.com>
2020-08-02 21:07:24 -0400

    additions

--- javapractice/src/examples/PracticeStream.java
+++ javapractice/src/examples/PracticeStream.java
@@ -2,6 +2,7 @@ package examples;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeStream {
     public static void main(String[] args) {
@@ -14,5 +15,12 @@ public class PracticeStream {
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);
+
+        List<String> names =
+                Arrays.asList("Reflection","Collection","Stream");
+
+        List<String> show =
+                names.stream().sorted().collect(Collectors.toList());
+        System.out.println(show);
     }
 }
